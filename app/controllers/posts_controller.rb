e5f4b3de418c248952062385ec3df35988db0e27
Tigran Terian <tigran.terian@gmail.com>
2015-12-23 11:22:25 +0400

    posts and pages controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+  
+  # GET /api/posts
+  # Get all the posts
+  def index
+    posts = Post.all
+    render json: posts
+  end
+
+  # GET /api/posts/:id
+  # Get a specific post
+  def show
+    post = Post.find(params[:id])
+    render json: post
+  end
+
+  # POST /api/posts
+  # Add a new post
+  def create
+    post = Post.new(post_params)
+
+    if post.save
+      render json: post
+    else
+      render json: { error: "Post creating error" }, status: :unprocessable_entity
+    end
+  end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :user_id)
+  end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root 'pages#home'
   scope '/api' do
     mount_devise_token_auth_for 'User', at: '/auth'
+    resources :posts,         only: [:index, :show, :create] do
+      resources :comments,    only: [:index, :show, :create]
+    end
+    resources :relationships, only: [:create, :destroy]
   end
 
 end
