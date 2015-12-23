170cbeca63eae9ffe4012cde8f0a983cd730edc6
Tigran Terian <tigran.terian@gmail.com>
2015-12-23 11:52:31 +0400

    comment out unused modules

--- db/migrate/20151222061046_devise_token_auth_create_users.rb
+++ db/migrate/20151222061046_devise_token_auth_create_users.rb
@@ -9,11 +9,11 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration
       t.string :encrypted_password, :null => false, :default => ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      #t.string   :reset_password_token
+      #t.datetime :reset_password_sent_at
 
       ## Rememberable
-      t.datetime :remember_created_at
+      #t.datetime :remember_created_at
 
       ## Trackable
       t.integer  :sign_in_count, :default => 0, :null => false
@@ -23,10 +23,10 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration
       t.string   :last_sign_in_ip
 
       ## Confirmable
-      t.string   :confirmation_token
-      t.datetime :confirmed_at
-      t.datetime :confirmation_sent_at
-      t.string   :unconfirmed_email # Only if using reconfirmable
+      #t.string   :confirmation_token
+      #t.datetime :confirmed_at
+      #t.datetime :confirmation_sent_at
+      #t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
@@ -47,7 +47,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration
 
     add_index :users, :email
     add_index :users, [:uid, :provider],     :unique => true
-    add_index :users, :reset_password_token, :unique => true
+    # add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
   end
