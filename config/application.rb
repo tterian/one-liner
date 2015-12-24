1193a10189291f66c1f2ec52847bc6a21921c7df
Tigran Terian <tigran.terian@gmail.com>
2015-12-24 16:15:23 +0400

    preparing for deploy 1

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module Sharemj
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
     config.assets.paths << Rails.root.join("lib","assets","bower_components")
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
