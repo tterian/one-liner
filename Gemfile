d4dbfdc1aea53a399b9e64d6baaa110542e65ff7
Tigran Terian <tigran.terian@gmail.com>
2015-12-24 17:25:54 +0400

    asset precomile for heroku

--- Gemfile
+++ Gemfile
@@ -21,8 +21,6 @@ end
 
 group :production, :staging do
 	gem 'rails_12factor'
-	gem 'rails_stdout_logging'
-	gem 'rails_serve_static_assets'
 end
 
 
--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,6 @@ module Sharemj
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.initialize_on_precompile = false
     config.assets.paths << Rails.root.join("lib","assets","bower_components")
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
