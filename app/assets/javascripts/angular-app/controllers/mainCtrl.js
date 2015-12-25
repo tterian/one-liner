532c8dccc6c5133df274be3f37923071305f5520
Tigran Terian <tigran.terian@gmail.com>
2015-12-25 13:58:37 +0400

    clear up post creating flow

--- app/assets/javascripts/angular-app/controllers/mainCtrl.js
+++ app/assets/javascripts/angular-app/controllers/mainCtrl.js
@@ -2,6 +2,38 @@ function MainController($scope, $mdDialog, User, Post) {
 
   $scope.posts = Post.all;
 
+  $scope.addPost = function(post) {
+
+    if ($scope.posts.length == 0) {
+      var lastPost = 0;
+    } else {
+      var lastPost = $scope.posts[$scope.posts.length - 1];
+    }
+
+    var newPost = {
+      id:           lastPost.id + 1,
+      content:      post.content,
+      user_id:      $scope.user.id,
+      user:         $scope.user,
+      created_at:   new Date(),
+      comments:     []
+    };
+
+    console.log(newPost);
+    console.log(angular.copy(newPost));
+
+    Post.create(newPost).$promise
+      .then(function() {
+        $scope.posts.push(angular.copy(newPost));
+        $scope.post = '';
+      });
+  };
+
+
+
+
+
+
   $scope.showSignIn = function(ev) {
     $mdDialog.show({
       scope: $scope,
@@ -20,17 +52,6 @@ function MainController($scope, $mdDialog, User, Post) {
       escapeToClose: true,
       controller: 'UsersController',
       templateUrl: 'assets/angular-app/templates/user/signup.html.erb',
-      targetEvent: ev
-    });
-  };
-
-  $scope.writePost = function(ev) {
-    $mdDialog.show({
-      scope: $scope,
-      preserveScope: true,
-      escapeToClose: true,
-      controller: 'PostsController',
-      templateUrl: 'assets/angular-app/templates/post/new.html.erb',
       targetEvent: ev
     });
   };
