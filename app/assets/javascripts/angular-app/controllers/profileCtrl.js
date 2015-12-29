05f7758a54d9ec244174b99db07f2fd3c0fabcaa
Tigran Terian <tigran.terian@gmail.com>
2015-12-29 17:28:57 +0400

    multi-user bugfixes

--- app/assets/javascripts/angular-app/controllers/profileCtrl.js
+++ app/assets/javascripts/angular-app/controllers/profileCtrl.js
@@ -2,31 +2,6 @@ function ProfilesController($scope, $routeParams, $location, $mdDialog, User, Ac
 
   $scope.posts = Post.all;
   $scope.currentUser = Account.get($routeParams.id);
-  console.log($scope.currentUser);
-
-  $scope.addPost = function(post) {
-
-    if ($scope.posts.length == 0) {
-      var lastPost = 0;
-    } else {
-      var lastPost = $scope.posts[$scope.posts.length - 1];
-    }
-
-    var newPost = {
-      id:           lastPost.id + 1,
-      content:      post.content,
-      user_id:      $scope.user.id,
-      user:         $scope.user,
-      created_at:   new Date(),
-      comments:     []
-    };
-
-    Post.create(newPost).$promise
-      .then(function() {
-        $scope.posts.push(newPost);
-        $scope.post = '';
-      });
-  };
 
   $scope.deletePost = function(post) {
 
