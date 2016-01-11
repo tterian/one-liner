40ac0adbdb238f3708d8bdded9b296bdb6b6c72d
Tigran Terian <tigran.terian@gmail.com>
2016-01-11 15:54:07 +0400

    follow main page

--- app/assets/javascripts/angular-app/controllers/mainCtrl.js
+++ app/assets/javascripts/angular-app/controllers/mainCtrl.js
@@ -1,6 +1,30 @@
-function MainController($scope, $location, $mdDialog, User, Post) {
+function MainController($scope, $location, $mdDialog, User, Relationship, Suggestion, Post) {
 
   $scope.posts = Post.all;
+  $scope.suggestions = Suggestion.all;
+
+  $scope.isFollowedBy = function(userId, followers) {
+    for ( var i = 0; i < followers.length; i++ ) {
+      if ( followers[i].id == userId ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  $scope.followUser = function(followedId) {
+    Relationship.follow(followedId).$promise
+      .then(function() {
+        $scope.user.following_count = $scope.user.following_count + 1;
+      });
+  }
+
+  $scope.unfollowUser = function(followedId) {
+    Relationship.unfollow(followedId).$promise
+      .then(function() {
+        $scope.user.following_count = $scope.user.following_count - 1;
+      });
+  }
 
   $scope.addPost = function(post) {
     var user = $scope.user;
@@ -42,6 +66,21 @@ function MainController($scope, $location, $mdDialog, User, Post) {
     return new Array(Math.round(num * 100) / 100);   
   }
 
+  $scope.calculateStarNumber = function(rating) {
+    if (rating.count == 0) {
+      return new Array(0)
+    } else {      
+      return new Array(Math.round(rating.sum/rating.count * 100) / 100);
+    }
+  }
+
+  $scope.calculateStarNumberN = function(rating) {
+    if (rating.count == 0) {
+      return new Array(5)
+    } else {      
+      return new Array(5 - Math.round(rating.sum/rating.count * 100) / 100);
+    }
+  }
 
   $scope.signIn = function(user) {
     User.signIn(user)
